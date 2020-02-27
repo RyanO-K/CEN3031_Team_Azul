b2c6d2fbf24df372228ccc54f1b1a3235fb038ea
Ryan O'Keefe <rokeefe@ufl.edu>
2020-02-27 17:46:55 -0500

    Delete config.js
    production database is now online so we cant keep using a config file with it stored on github

--- server/config/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    db: {
-      uri: 'mongodb+srv://katieohern:beastcat@heroku-vvdkq.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    }
-  };
