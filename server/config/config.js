158eb393fd61746da7bd40a6c036d81ab3b94c11
katieohern <katieohern@gmail.com>
2020-03-12 13:01:07 -0400

    changes for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
-export default{
+module.exports = {
     db: {
         uri: 'mongodb+srv://katieohern:beastcat@heroku-vvdkq.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.'
     }
-}
+};
--- server/config/express.js
+++ server/config/express.js
@@ -1,44 +1,41 @@
-import path from 'path';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import express from 'express';
-import mongoose from 'mongoose';
-import uri from './config.js';
-import exampleRouter from '../routes/userRoutes.js';
+    var path = require('path');
+    var bodyParser = require('body-parser');
+    var morgan = require('morgan');
+    var express = require('express');
+    var mongoose = require('mongoose');
+    var exampleRouter = require('../routes/userRoutes.js');
+    var uri = require('./config.js');
 
-export default {
-init: () => {
-    /* 
-        connect to database
-        - reference README for db uri
-    */
-    mongoose.connect(String(uri.db.uri),{useNewUrlParser: true,useUnifiedTopology: true});
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useFindAndModify', false);
+module.exports.init = () => {
+        /* 
+            connect to database
+            - reference README for db uri
+        */
+        mongoose.connect(String(uri.db.uri),{useNewUrlParser: true,useUnifiedTopology: true});
+        mongoose.set('useCreateIndex', true);
+        mongoose.set('useFindAndModify', false);
 
-    // initialize app
-    const app = express();
+        // initialize app
+        const app = express();
 
-    // enable request logging for development debugging
-    app.use(morgan('dev'));
+        // enable request logging for development debugging
+        app.use(morgan('dev'));
 
-    // body parsing middleware
-    app.use(bodyParser.json());
+        // body parsing middleware
+        app.use(bodyParser.json());
 
-    // add a router
-    app.use('/api', exampleRouter);
+        // add a router
+        app.use('/api/horoscopeInfo', exampleRouter);
 
-    if (process.env.NODE_ENV === 'production') {
-        // Serve any static files
-        app.use(express.static(path.join(__dirname, '../../client/build')));
+        if (process.env.NODE_ENV === 'production') {
+            // Serve any static files
+            app.use(express.static(path.join(__dirname, '../../client/build')));
 
-        // Handle React routing, return all requests to React app
-        app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-        });
-        }
+            // Handle React routing, return all requests to React app
+            app.get('*', function(req, res) {
+                res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+            });
+            }
 
-    return app
-}
-
-}
+        return app
+    }
