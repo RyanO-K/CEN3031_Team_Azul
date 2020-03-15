cf6f33ed8aefd5e262cda9b039579666241c28ff
mjmcallister99 <36482982+mjmcallister99@users.noreply.github.com>
2020-03-15 18:16:34 -0400

    Update config.js
    Added test database

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,8 @@
 module.exports = {
     db: {
         uri: 'mongodb+srv://katieohern:beastcat@heroku-vvdkq.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.'
+    },
+    test_db: {
+        uri: 'mongodb+srv://user:gators123@testing-yclvl.mongodb.net/test?retryWrites=true&w=majority',
     }
 };
