f6d384e9fb4fde5c42b6220ce30052dd482a1916
Topher <schaeferchris08@gmail.com>
2020-03-29 16:09:36 -0400

    Admin page: dropdown lists

--- client/src/views/Admin/Admin.js
+++ client/src/views/Admin/Admin.js
@@ -1,75 +1,129 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles({
-    table: {
-      minWidth: 650,
-    },
-  });
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
-function createData(Day, Aquarius, Pisces, Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn){
-    return{Day, Aquarius, Pisces, Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn}
+const Admin = () =>{
+    const dispSunSign = (clicked) =>
+    {
+        console.log(clicked.target.id)
+    }
+    const dispMoonPhase = (clicked) =>
+    {
+        console.log(clicked.target.id)
+    }
+    const dispHouses = (clicked) =>
+    {
+        console.log(clicked.target.id)
+    }
+    return(
+<div>
+    <DropdownButton id="dropdown-basic-button" title="Sun Signs">
+        <div>
+            <Dropdown.Item as="button" id="Aquarius" onClick={(e)=>dispSunSign(e)}>Aquarius</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Aries" onClick={(e)=>dispSunSign(e)}>Aries</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Taurus" onClick={(e)=>dispSunSign(e)}>Taurus</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Gemini" onClick={(e)=>dispSunSign(e)}>Gemini</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Cancer" onClick={(e)=>dispSunSign(e)}>Cancer</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Leo" onClick={(e)=>dispSunSign(e)}>Leo</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Virgo" onClick={(e)=>dispSunSign(e)}>Virgo</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Libra" onClick={(e)=>dispSunSign(e)}>Libra</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Scorpio" onClick={(e)=>dispSunSign(e)}>Scorpio</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Sagittarius" onClick={(e)=>dispSunSign(e)}>Sagitarrius</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Capricorn" onClick={(e)=>dispSunSign(e)}>Capricon</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Pisces" onClick={(e)=>dispSunSign(e)}>Pisces</Dropdown.Item>
+        </div>
+
+    </DropdownButton>
+
+    <DropdownButton id="dropdown-basic-button" title="Lunar Phase">
+        <div>
+            <Dropdown.Item as="button" id="New Moon" onClick={(e)=>dispMoonPhase(e)}>New Moon</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Waxing Crescent" onClick={(e)=>dispMoonPhase(e)}>Waxing Crescent</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="First Quarter" onClick={(e)=>dispMoonPhase(e)}>First Quarter</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Waxing Gibbous" onClick={(e)=>dispMoonPhase(e)}>Waxing Gibbous</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Full Moon" onClick={(e)=>dispMoonPhase(e)}>Full Moon</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Waning Gibbous" onClick={(e)=>dispMoonPhase(e)}>Waning Gibbous</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Last Quarter" onClick={(e)=>dispMoonPhase(e)}>Last Quarter</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="Waning Crescent" onClick={(e)=>dispMoonPhase(e)}>Waning Crescent</Dropdown.Item>
+        </div>
+    </DropdownButton>
+    <DropdownButton id="dropdown-basic-button" title="Houses">
+        <div>
+            <Dropdown.Item as="button" id="1st" onClick={(e)=>dispHouses(e)}>1st</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="2nd" onClick={(e)=>dispHouses(e)}>2nd</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="3rd" onClick={(e)=>dispHouses(e)}>3rd</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="4th" onClick={(e)=>dispHouses(e)}>4th</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="5th" onClick={(e)=>dispHouses(e)}>5th</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="6th" onClick={(e)=>dispHouses(e)}>6th</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="7th" onClick={(e)=>dispHouses(e)}>7th</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="8th" onClick={(e)=>dispHouses(e)}>8th</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="9th" onClick={(e)=>dispHouses(e)}>9th</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="10th" onClick={(e)=>dispHouses(e)}>10yj</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="11th" onClick={(e)=>dispHouses(e)}>11th</Dropdown.Item>
+        </div>
+        <div>
+            <Dropdown.Item as="button" id="12th" onClick={(e)=>dispHouses(e)}>12th</Dropdown.Item>
+        </div>
+    </DropdownButton>
+    </div>   
+);
+
 }
-
-const rows = [
-    createData('March 1st', 'This', 'Is', 'All', 'Test', 'And', 'Example', 'Data', 'I\'m a Virgo', 'What are you?', 'Scorpio data', 'Capricorn Data', 'Wow lotta info'),
-
-];
-export default function Admin(){
-    const classes = useStyles;
-
-    return (
-        <TableContainer component = {Paper}>
-            <Table className={classes.table} aria-label="Heavenly Table">
-                <caption>A Heavenly Horoscope Header, Honey!</caption>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Day</TableCell>
-                        <TableCell align="right">Aquarius</TableCell>
-                        <TableCell align="right">Pisces</TableCell>
-                        <TableCell align="right">Aries</TableCell>
-                        <TableCell align="right">Taurus</TableCell>
-                        <TableCell align="right">Gemini</TableCell>
-                        <TableCell align="right">Cancer</TableCell>
-                        <TableCell align="right">Leo</TableCell>
-                        <TableCell align="right">Virgo</TableCell>
-                        <TableCell align="right">Libra</TableCell>
-                        <TableCell align="right">Scorpio</TableCell>
-                        <TableCell align="right">Sagittarius</TableCell>
-                        <TableCell align="right">Capricorn</TableCell>
-
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) =>(
-                        <TableRow key = {row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.Aquarius}</TableCell>
-                        <TableCell align="right">{row.Pisces}</TableCell>
-                        <TableCell align="right">{row.Aries}</TableCell>
-                        <TableCell align="right">{row.Taurus}</TableCell>
-                        <TableCell align="right">{row.Gemini}</TableCell>
-                        <TableCell align="right">{row.Cancer}</TableCell>
-                        <TableCell align="right">{row.Leo}</TableCell>
-                        <TableCell align="right">{row.Virgo}</TableCell>
-                        <TableCell align="right">{row.Libra}</TableCell>
-                        <TableCell align="right">{row.Scorpio}</TableCell>
-                        <TableCell align="right">{row.Sagittarius}</TableCell>
-                        <TableCell align="right">{row.Capricorn}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
-
-}
+export default Admin;
