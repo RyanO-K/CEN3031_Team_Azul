68d6e308ac397852228f9d31312aa6ef49928d1f
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-03-29 00:28:29 -0400

    Logout from User page

--- client/src/views/SignUp/User.js
+++ client/src/views/SignUp/User.js
@@ -8,6 +8,8 @@ import { useForm } from 'react-hook-form'
 import SignUpWithGoogle from "./SignUpWithGoogle";
 import SignUp2 from "./SignUp2";
 import UserProfile from './UserState';
+import  {GoogleLogin, GoogleLogout}  from 'react-google-login';
+import config from './config.json';
 
 
 
@@ -33,18 +35,30 @@ function User(){
         name: UserProfile.getLocalStorageName(),
         pob: UserProfile.getLocalStorageBPlace(),
         dob: UserProfile.getLocalStorageBDay(),
-        email: UserProfile.getLocalStorageEmail()
+        email: UserProfile.getLocalStorageEmail(),
+        b:false
     });
+    //if(newUser.name!==null && newUser.name.length===0)
+      //  window.location.reload();
     
     const classes = useStyles();
 
 
     function handle(){
+        console.log("Hi");
         UserProfile.setEmail('');
         UserProfile.setName('');
         UserProfile.loggingOut();
         UserProfile.setBirthday('');
         UserProfile.setBirthplace('');
+        let a={
+            name: '',
+            pob: '',
+            dob: '',
+            email: '',
+            b:true
+            };
+            setNewUser(a);
         UserProfile.setLocalStorageBDay();
         UserProfile.setLocalStorageBPlace();
         UserProfile.setLocalStorageEmail();
@@ -53,15 +67,25 @@ function User(){
         UserProfile.setLocalStorageisLoggedInWithGoogle();
         UserProfile.setLocalStorageisLoggedInWithoutGoogle();
 
-        let a={
+        let q={
         name: '',
         pob: '',
         dob: '',
-        email: ''
+        email: '',
+        b:true
         };
-        setNewUser(a);
+        setNewUser(q);
 
     }
+    console.log(newUser.name);
+    if(newUser.b && UserProfile.getLocalStorageName()!==''){
+        console.log(newUser.b);
+    return( <Redirect to={{pathname:'/Home', 
+}}/>
+);
+    }
+    else if(UserProfile.getLocalStorageisLoggedInWithGoogle()){//google login
+        console.log("Google");
     return(
   <p>
         {newUser.name}<br></br>
@@ -70,9 +94,33 @@ function User(){
         {newUser.pob}<br></br>
         <br></br>
         <div>
-                    <ColorButton onClick={handle} className={classes.margin} component={Link} size="large" variant="outlined" to={{pathname: '/Home'}}>Log Out</ColorButton>
-                        </div>
+
+
+        <GoogleLogout 
+        onLogoutSuccess={handle}
+                   clientId={config.GOOGLE_CLIENT_ID}
+                   theme="dark"
+                 
+         />                        </div>
    </p>
     );
+   
+                   } 
+                   else if(UserProfile.getLocalStorageisLoggedInWithoutGoogle()) {//regular login
+                       return(
+                        <p>
+                        {newUser.name}<br></br>
+                        {newUser.email}<br></br>
+                        {newUser.dob}<br></br>
+                        {newUser.pob}<br></br>
+                        <br></br>
+                        <div>
+                        <ColorButton onClick={handle} className={classes.margin} component={Link} size="large" variant="outlined" to={{pathname: '/Home'}}>Log Out</ColorButton>
+                      </div>
+                       </p>
+                        ); }
+                        else{
+                            return <p>{newUser.name}</p>;
+                        }
 }
 export default User;
