953080cf04a1d7823ab740d7de9ad688d3f74c79
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-03-28 21:35:46 -0400

    Session

--- client/src/views/SignUp/SignUpWithGoogle.js
+++ client/src/views/SignUp/SignUpWithGoogle.js
@@ -2,6 +2,7 @@ import  GoogleLogin  from 'react-google-login';
 import React, {useState, useEffect, Component} from 'react';
 import {Redirect} from 'react-router-dom';
 import config from './config.json';
+import UserProfile from './UserState.js';
 
 class LoginWithGoogle extends Component {
    
@@ -13,10 +14,17 @@ class LoginWithGoogle extends Component {
     googleResponse = (response) => {
         //if response.profileObj.email already exists in database, then don't allow them to sign up because theyve done it before
         //Other than that, requests only need to be worried about for google sign up in the signup2.js file
-    if(this.state.name!=='')
-        alert("you are already logged in with email " +this.state.email+".  Please log out if you would like to login with another account.");
+    if(UserProfile.getLocalStorageisLoggedIn())
+        alert("you are already logged in with email " +UserProfile.getEmail()+".  Please log out if you would like to login with another account.");
     else{
+    UserProfile.setName(response.profileObj.name);
+    UserProfile.setEmail(response.profileObj.email);
+    UserProfile.loggingInWithGoogle();
+    UserProfile.setLocalStorageEmail();
+    UserProfile.setLocalStorageisLoggedInWithGoogle();
+    UserProfile.setLocalStorageName();
     this.setState({name:response.profileObj.name, email:response.profileObj.email, pob:'', dob:'', loggedIn:true})
+    
     console.log(this.state.name);
     console.log(this.state.email);
     }
@@ -25,34 +33,26 @@ class LoginWithGoogle extends Component {
     render() {
         if (this.state.loggedIn) {
             return( <Redirect to={{pathname:'/SignUp2', 
-            state:{name:this.state.name, email:this.state.email} }}/>
+            }}/>
             );
           }
-        let content = !!this.state.loggedIn ?
-            (
-                <div>
-                    <p>Authenticated</p>
-                    <div>
-                        {this.state.email}
-                    </div>
-                </div>
-            ) :
-            (
+       let content=
                 <div>
                    
                     <GoogleLogin
+                    
                         theme="dark"
   
                         clientId={config.GOOGLE_CLIENT_ID}
                         buttonText="Sign Up"
-                        
+                        disabled={!(UserProfile.getLocalStorageisLoggedIn)}
                         onSuccess={this.googleResponse}
                         onFailure={this.onFailure}
                         cookiePolicy={'single_host_origin'}
                     />
                         
                 </div>
-            );
+            
 
         return (
             <div className="LoginWithGoogle">
