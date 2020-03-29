25e2bc99afc0b686fb2b8edfe523409e57aa8827
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-03-29 15:30:59 -0400

    added time field

--- client/src/views/SignUp/SignUp2.js
+++ client/src/views/SignUp/SignUp2.js
@@ -35,11 +35,14 @@ function SignUp2(props) {
         name: props.location.state.name,
         pob: '',
         dob: '',
-        email: props.location.state.name
+        tob:'',
+        email: props.location.state.email
+        
     });
     const [problem, setProblem] = useState({
         pobP: false,
-        dobP: false
+        dobP: false,
+        tobP: false
     });
     const [destination,d]=useState("/SignUp2");
 
@@ -52,7 +55,8 @@ function SignUp2(props) {
             name:newUser.name,
             pob: data.pob,
             dob: data.dob,
-            email:newUser.email
+            email:newUser.email,
+            tob:newUser.tob
         }
         console.log("User"+data);
         // {...newUser,
@@ -88,6 +92,12 @@ function SignUp2(props) {
             console.log("pob err");
         }
 
+        if(newUser.tob.length===0){
+            problem.tobP=true;
+            bool=true;
+            console.log("tob err");
+        }
+
 
 
         let err="";
@@ -96,12 +106,21 @@ function SignUp2(props) {
             if(problem.pobP){
                 err+="No place of birth given\n";
                 problem.pobP=false;
+
             }
 
             if(problem.dobP){
                 err+="No date of birth given\n";
                 problem.dobP=false;
+
             }
+
+            if(problem.tobP){
+                err+="No time of birth given\n";
+                problem.tobP=false;
+
+            }
+            
 
             alert(err);
             
@@ -110,6 +129,8 @@ function SignUp2(props) {
                         UserProfile.setName(props.location.state.name);
                         UserProfile.setEmail(props.location.state.email);
                         UserProfile.loggingInWithGoogle();
+                        UserProfile.setBirthTime(newUser.tob);
+                        UserProfile.setLocalStorageBTime();
                         UserProfile.setLocalStorageEmail();
                         UserProfile.setLocalStorageisLoggedInWithGoogle();
                         UserProfile.setLocalStorageName();
@@ -121,7 +142,7 @@ function SignUp2(props) {
 
 
    const func=(a)=>{
-    if(a.length==10 && newUser.pob.length>0)
+    if(a.length==10 && newUser.pob.length>0 && newUser.tob.length>0)
     d('/User');
 else
     d('/SignUp2');
@@ -129,15 +150,16 @@ else
         name:newUser.name,
         pob: newUser.pob,
         dob: a,
-        email:newUser.email
+        email:newUser.email,
+        tob: newUser.tob
     }
     UserProfile.setBirthday(a);
     setNewUser(user);
     };
 
     const func2=(b)=>{
-
-        if(newUser.dob.length==10 && b.length>0)
+console.log(newUser);
+        if(newUser.dob.length==10 && b.length>0 && newUser.tob.length>0)
         d('/User');
     else
         d('/SignUp2');
@@ -145,12 +167,30 @@ else
             name:newUser.name,
             pob: b,
             dob: newUser.dob,
-            email:newUser.email
+            email:newUser.email,
+            tob:newUser.tob
         }
         UserProfile.setBirthplace(b);
         setNewUser(user);
         };
 
+        const func6=(efg)=>{
+            if(newUser.dob.length==10 && newUser.pob.length>0 && efg.length>0)
+d('/User');
+else
+d('/SignUp2');
+            const user={
+                name:newUser.name,
+                pob: newUser.pob,
+                dob: newUser.dob,
+                email:newUser.email,
+                password:newUser.password,
+                tob:efg
+            }
+            if(UserProfile.getLocalStorageisLoggedIn()===true)
+d('/SignUp2');
+            setNewUser(user);
+            };
     const classes = useStyles();
 
     return (
@@ -175,8 +215,13 @@ else
                     <div>
                         <input type="text" placeholder="Place of Birth" name="pob" ref={register} onChange={(e)=>func2(e.target.value)}/>
                     </div>               
+
                     <div>
-                    <ColorButton onClick={handle} className={classes.margin} component={Link} size="large" variant="outlined" to={{pathname:destination}}> Submit</ColorButton>
+                        <input type="text" placeholder="Time of Birth" name="tob" ref={register} onChange={(e)=>func6(e.target.value)}/>
+                    </div>      
+
+                    <div>
+                    <ColorButton onClick={handle} className={classes.margin} component={Link} size="large" variant="outlined" to={{pathname:destination, state:{name:newUser.name, email:newUser.email}}}> Submit</ColorButton>
                         
                         
                     </div>
