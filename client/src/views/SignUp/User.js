d64ca3b51134b0475d3dde9b20350bbb697165ec
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-03-29 16:56:23 -0400

    Update User.js

--- client/src/views/SignUp/User.js
+++ client/src/views/SignUp/User.js
@@ -7,23 +7,132 @@ import Button from '@material-ui/core/Button'
 import { useForm } from 'react-hook-form'
 import SignUpWithGoogle from "./SignUpWithGoogle";
 import SignUp2 from "./SignUp2";
+import UserProfile from './UserState';
+import  {GoogleLogin, GoogleLogout}  from 'react-google-login';
+import config from './config.json';
 
-function User(props){
-    console.log(props);
+
+
+const ColorButton = withStyles(theme => ({
+    root: {
+        padding: '6px 12px',
+        border: '1px solid',
+        backgroundColor: '#E28222',
+      '&:hover': {
+        backgroundColor: '#C6721D',
+      },
+    },
+}))(Button);
+
+const useStyles = makeStyles(theme => ({
+    margin: {
+      margin: theme.spacing(1),
+    },
+}));
+function User(){
+    console.log(UserProfile.getLocalStorageName());
+    const [st, newStat]=useState(0);
     const [newUser, setNewUser] = useState({
-        name: props.location.state.name,
-        pob: props.location.state.pob,
-        dob: props.location.state.dob,
-        email: props.location.state.email
+        name: UserProfile.getLocalStorageName(),
+        pob: UserProfile.getLocalStorageBPlace(),
+        dob: UserProfile.getLocalStorageBDay(),
+        email: UserProfile.getLocalStorageEmail(),
+        tob: UserProfile.getLocalStorageBTime(),
+        b:false
     });
+    //if(newUser.name!==null && newUser.name.length===0)
+      //  window.location.reload();
+      console.log(st);
+    const classes = useStyles();
+
+
+  function handle(){
+        console.log("Hi");
+        UserProfile.setEmail('');
+        UserProfile.setName('');
+        UserProfile.loggingOut();
+        UserProfile.setBirthday('');
+        UserProfile.setBirthplace('');
+        UserProfile.setBirthTime('');
+        let a={
+            name: '',
+            pob: '',
+            dob: '',
+            email: '',
+            tob:'',
+            b:true
+            };
+            setNewUser(a);
+            newStat(3);
+            setTimeout(3000);
+        UserProfile.setLocalStorageBTime();
+        UserProfile.setLocalStorageBDay();
+        UserProfile.setLocalStorageBPlace();
+        UserProfile.setLocalStorageEmail();
+        UserProfile.setLocalStorageName();
+        UserProfile.setLocalStorageisLoggedIn();
+        UserProfile.setLocalStorageisLoggedInWithGoogle();
+        UserProfile.setLocalStorageisLoggedInWithoutGoogle();
+        newStat(2);
+
+        let r={
+            name: '',
+            pob: '',
+            dob: '',
+            email: '',
+            tob:'',
+            b:true
+            };
+            setNewUser(r);
+            console.log(UserProfile.getLocalStorageisLoggedIn())
+
+    }
+    console.log(newUser.name);
+    if(newUser.b && UserProfile.getLocalStorageName()!==''){
+        console.log(newUser.b);
+    return( <Redirect to={{pathname:'/Home', 
+}}/>
+);
+    }
+    else if(UserProfile.getLocalStorageisLoggedInWithGoogle()){//google login
+        console.log("Google");
     return(
   <p>
         {newUser.name}<br></br>
         {newUser.email}<br></br>
         {newUser.dob}<br></br>
         {newUser.pob}<br></br>
+        {newUser.tob}<br></br>
+        <br></br>
+        <div>
 
+
+        <GoogleLogout 
+        onLogoutSuccess={handle}
+                   clientId={config.GOOGLE_CLIENT_ID}
+                   theme="dark"
+                 
+         />                        </div>
    </p>
     );
+   
+                   } 
+                   else if(UserProfile.getLocalStorageisLoggedInWithoutGoogle()) {//regular login
+                       return(
+                        <p>
+                        {newUser.name}<br></br>
+                        {newUser.email}<br></br>
+                        {newUser.dob}<br></br>
+                        {newUser.pob}<br></br>
+                        {newUser.tob}<br></br>
+                        <br></br>
+                        <div>
+                        <ColorButton onClick={handle} className={classes.margin} component={Link} size="large" variant="outlined" to={{pathname: '/Home'}}>Log Out</ColorButton>
+                      </div>
+                       </p>
+                        ); }
+                        else{
+                            return <p>{newUser.name}</p>;
+                        }
 }
 export default User;
