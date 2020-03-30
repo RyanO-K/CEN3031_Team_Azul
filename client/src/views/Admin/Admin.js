e117515b8f717d64b545c2894bc29e9e46fb0214
Kim-Andrew <44186827+Kim-Andrew@users.noreply.github.com>
2020-03-30 17:53:15 -0400

    button side by side, text box to right

--- client/src/views/Admin/Admin.js
+++ client/src/views/Admin/Admin.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {textareaAutosize, FilledInput, FormControl, FormHelperText, Input, InputAdornment, InputBase, InputLabel, OutlinedInput, TextField} from '@material-ui/core'
+import {TextField} from '@material-ui/core'
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import UserProfile from '../SignUp/UserState';
-import axiosPath from '../../axiosRequests'
+import axiosPath from '../../axiosRequests';
+import './Admin.css';
 
 const Admin = () =>{
 
@@ -90,132 +91,149 @@ const Admin = () =>{
         
         
     }
-    return(
-<div>
-    <DropdownButton  id="dropdown-basic-button" title="Sun Signs">
-        <div>
-            <Dropdown.Item as="button" id="Aquarius" onClick={(e)=>dispSunSign(e)}>Aquarius</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Aries" onClick={(e)=>dispSunSign(e)}>Aries</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Taurus" onClick={(e)=>dispSunSign(e)}>Taurus</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Gemini" onClick={(e)=>dispSunSign(e)}>Gemini</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Cancer" onClick={(e)=>dispSunSign(e)}>Cancer</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Leo" onClick={(e)=>dispSunSign(e)}>Leo</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Virgo" onClick={(e)=>dispSunSign(e)}>Virgo</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Libra" onClick={(e)=>dispSunSign(e)}>Libra</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Scorpio" onClick={(e)=>dispSunSign(e)}>Scorpio</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Sagittarius" onClick={(e)=>dispSunSign(e)}>Sagitarrius</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Capricorn" onClick={(e)=>dispSunSign(e)}>Capricon</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="Pisces" onClick={(e)=>dispSunSign(e)}>Pisces</Dropdown.Item>
-        </div>
+    return (
 
-    </DropdownButton>
 
-    <DropdownButton id="dropdown-basic-button" title="Lunar Phase">
         <div>
-            <Dropdown.Item as="button" id="NewMoon" onClick={(e)=>dispMoonPhase(e)}>New Moon</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="WaxingCrescent" onClick={(e)=>dispMoonPhase(e)}>Waxing Crescent</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="FirstQuarter" onClick={(e)=>dispMoonPhase(e)}>First Quarter</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="WaxingGibbous" onClick={(e)=>dispMoonPhase(e)}>Waxing Gibbous</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="FullMoon" onClick={(e)=>dispMoonPhase(e)}>Full Moon</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="WaningGibbous" onClick={(e)=>dispMoonPhase(e)}>Waning Gibbous</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="LastQuarter" onClick={(e)=>dispMoonPhase(e)}>Last Quarter</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="WaningCrescent" onClick={(e)=>dispMoonPhase(e)}>Waning Crescent</Dropdown.Item>
-        </div>
-    </DropdownButton>
-    <DropdownButton id="dropdown-basic-button" title="Houses">
-        <div>
-            <Dropdown.Item as="button" id="1st" onClick={(e)=>dispHouses(e)}>1st</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="2nd" onClick={(e)=>dispHouses(e)}>2nd</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="3rd" onClick={(e)=>dispHouses(e)}>3rd</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="4th" onClick={(e)=>dispHouses(e)}>4th</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="5th" onClick={(e)=>dispHouses(e)}>5th</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="6th" onClick={(e)=>dispHouses(e)}>6th</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="7th" onClick={(e)=>dispHouses(e)}>7th</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="8th" onClick={(e)=>dispHouses(e)}>8th</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="9th" onClick={(e)=>dispHouses(e)}>9th</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="10th" onClick={(e)=>dispHouses(e)}>10th</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="11th" onClick={(e)=>dispHouses(e)}>11th</Dropdown.Item>
-        </div>
-        <div>
-            <Dropdown.Item as="button" id="12th" onClick={(e)=>dispHouses(e)}>12th</Dropdown.Item>
-        </div>
-    </DropdownButton>
-    <div>
-        <div>sun: {data.sun}</div>
-        <div>moon: {data.moon}</div>
-        <div>house: {data.house}</div>
-        <div>interpretation: {data.interpretation}</div>
-        <form onSubmit={handleSubmit}> 
-            <div>
-                <TextField
-                    id="standard-multiline-static"
-                    onChange={handleChange}
-                    label="Interpretation"
-                    multiline
-                    rows="4"
-                />
+            <div className='column1'>
+                <div>
+                    <div className="flot1">
+                        <DropdownButton id="dropdown-basic-button" title="Sun Signs">
+                            <div>
+                                <Dropdown.Item as="button" id="Aquarius" onClick={(e) => dispSunSign(e)}>Aquarius</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Aries" onClick={(e) => dispSunSign(e)}>Aries</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Taurus" onClick={(e) => dispSunSign(e)}>Taurus</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Gemini" onClick={(e) => dispSunSign(e)}>Gemini</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Cancer" onClick={(e) => dispSunSign(e)}>Cancer</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Leo" onClick={(e) => dispSunSign(e)}>Leo</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Virgo" onClick={(e) => dispSunSign(e)}>Virgo</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Libra" onClick={(e) => dispSunSign(e)}>Libra</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Scorpio" onClick={(e) => dispSunSign(e)}>Scorpio</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Sagittarius" onClick={(e) => dispSunSign(e)}>Sagitarrius</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Capricorn" onClick={(e) => dispSunSign(e)}>Capricon</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="Pisces" onClick={(e) => dispSunSign(e)}>Pisces</Dropdown.Item>
+                            </div>
+
+                        </DropdownButton>
+                    </div>
+                    <div className="flot1">
+                        <DropdownButton id="dropdown-basic-button" title="Lunar Phase">
+                            <div>
+                                <Dropdown.Item as="button" id="NewMoon" onClick={(e) => dispMoonPhase(e)}>New Moon</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="WaxingCrescent" onClick={(e) => dispMoonPhase(e)}>Waxing Crescent</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="FirstQuarter" onClick={(e) => dispMoonPhase(e)}>First Quarter</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="WaxingGibbous" onClick={(e) => dispMoonPhase(e)}>Waxing Gibbous</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="FullMoon" onClick={(e) => dispMoonPhase(e)}>Full Moon</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="WaningGibbous" onClick={(e) => dispMoonPhase(e)}>Waning Gibbous</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="LastQuarter" onClick={(e) => dispMoonPhase(e)}>Last Quarter</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="WaningCrescent" onClick={(e) => dispMoonPhase(e)}>Waning Crescent</Dropdown.Item>
+                            </div>
+                        </DropdownButton>
+                    </div>
+                    <div className="flot1">
+                        <DropdownButton id="dropdown-basic-button" title="Houses">
+                            <div>
+                                <Dropdown.Item as="button" id="1st" onClick={(e) => dispHouses(e)}>1st</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="2nd" onClick={(e) => dispHouses(e)}>2nd</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="3rd" onClick={(e) => dispHouses(e)}>3rd</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="4th" onClick={(e) => dispHouses(e)}>4th</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="5th" onClick={(e) => dispHouses(e)}>5th</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="6th" onClick={(e) => dispHouses(e)}>6th</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="7th" onClick={(e) => dispHouses(e)}>7th</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="8th" onClick={(e) => dispHouses(e)}>8th</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="9th" onClick={(e) => dispHouses(e)}>9th</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="10th" onClick={(e) => dispHouses(e)}>10th</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="11th" onClick={(e) => dispHouses(e)}>11th</Dropdown.Item>
+                            </div>
+                            <div>
+                                <Dropdown.Item as="button" id="12th" onClick={(e) => dispHouses(e)}>12th</Dropdown.Item>
+                            </div>
+                        </DropdownButton>
+                    </div>
+                </div>
             </div>
-            <input type="submit" value="Submit"/>
-        </form>
-    </div>
-    </div>   
-);
+
+            <div className="column2">
+
+                <div>sun: {data.sun}</div>
+                <div>moon: {data.moon}</div>
+                <div>house: {data.house}</div>
+                <div>interpretation: {data.interpretation}</div>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <TextField
+                            id="standard-multiline-static"
+                            onChange={handleChange}
+                            label="Interpretation"
+                            multiline
+                            rows="4"
+                        />
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
+
+            </div>
+        </div>
+
+
+
+    );
 
 }
 export default Admin;
