e91ac30a51f8b9ab5dd0d7d2bb838e15b1576f87
Ryan O'Keefe <44047602+rokeefe-UF@users.noreply.github.com>
2020-03-31 18:50:09 -0400

    I hate import vs require statements

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-//needs to be var if running local or import if heroku
+//needs to be var
 //var express = require('./config/express.js');
-import express from './config/express.js'
+
 // Use env port or default
 const port = process.env.PORT || 5000;
 //const port = 5000;
