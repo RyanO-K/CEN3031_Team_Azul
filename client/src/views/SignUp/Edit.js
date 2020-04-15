2150cbc2daebb5093668db1b0ad5336dca6f700c
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-04-15 15:35:34 -0400

    Update Edit.js

--- client/src/views/SignUp/Edit.js
+++ client/src/views/SignUp/Edit.js
@@ -1,27 +1,82 @@
-function Edit(){
+import { Redirect } from "react-router-dom";
+import React, {Component} from 'react';
+import { withRouter} from 'react-router-dom';
+import { Label, Input } from '@rebass/forms'
+import { Flex, Box, Button, Heading, Text, Link } from 'rebass';
+import UserProfile from './UserState';
+import axiosPath from '../../axiosRequests';
+
+class Edit extends Component{
+constructor(){
+    super();
+    this.state={name:UserProfile.getLocalStorageName(), bday:UserProfile.getLocalStorageBDay(), bplace: UserProfile.getLocalStorageBPlace(), btime: UserProfile.getLocalStorageBTime()};
+}
 
 
 
+async log2(){
+    const axiosUser = {
+        Name: this.state.name,
+        Sign: "Scorpio",
+        Birthday: this.state.bday,
+        TimeOfBirth: this.state.btime,
+        LocationOfBirth: this.state.bplace,
+        Email: UserProfile.getLocalStorageEmail(),
+        Password:'testPassword'
+    }
+    console.log(axiosUser);
+  const a=
+  await (axiosPath.makeUpdateRequest('personal/'+UserProfile.getLocalStorageEmail(), axiosUser));
+  console.log(a);
+UserProfile.setName(this.state.name);
+UserProfile.setBirthday(this.state.bday);
+UserProfile.setBirthplace(this.state.bplace);
+UserProfile.setBirthTime(this.state.btime);
+UserProfile.setLocalStorageName();
+UserProfile.setLocalStorageBDay();
+UserProfile.setLocalStorageBPlace();
+UserProfile.setLocalStorageBTime();
+return a;
+   };
 
+handleSubmit = async (event) => {
+    event.preventDefault();
+    const { name, btime, bplace, bday } = this.state;
+    if(name.length===0)
+        alert("Name field cannot be empty");
+    else if(bday.length!==10)
+        alert("Must have valid birth date");
+    else{
+        await this.log2();
+        console.log("update successful");
+    }
+}
 
+handleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
+render(){
+    if(UserProfile.getLocalStorageEmail()===''||UserProfile.getLocalStorageEmail()===null || UserProfile.getLocalStorageEmail()==='null'||UserProfile.getLocalStorageEmail()===undefined)
+        return (<Redirect to='/Home'/>);
+ const {name, bday, bplace, btime, error } = this.state;
+ return(
     <div className="Signin-card">
     <p></p>
-    <input type="text" placeholder="name" value={UserState.getLocalStorageName()} name="name" ref={register} onChange={(e)=>func5(e.target.value)}/>
+   
+    <form on onSubmit={this.handleSubmit}>
+             <Input type="text" name="name" placeholder="Name" value={name} onChange={this.handleInputChange} />
+             <Input type="date" name="bday" placeholder="Birth Date" value={bday} onChange={this.handleInputChange} />
+             <Input type="text" name="btime" placeholder="Birth Time" value={btime} onChange={this.handleInputChange} />
+             <Input type="text" name="bplace" placeholder="Birth Place" value={bplace} onChange={this.handleInputChange} />
+             <Button children="Log In" />
+           </form>
     <div>
-        <input type="email" placeholder="Email Address"value={UserState.getLocalStorageEmail()} name="email" ref={register}/>
-    </div>
-    <div>
-        <input type="text" placeholder="Place of Birth"value={UserState.getLocalStorageBDay()} name="pob" ref={register} onChange={(e)=>func2(e.target.value)}/>
-    </div>
-    <div>
-        <input type="text" placeholder="Time of Birth" value={UserState.getLocalStorageBTime()} name="tob" ref={register} onChange={(e)=>func6(e.target.value)}/>
-    </div>
-    <div>
-    <input type="date" placeholder="Date of Birth" value={UserState.getLocalStorageBPlace()} name="dob" ref={register} onChange={(e)=>func(e.target.value)} />
-    </div>
-    <div>
-    <ColorButton onClick={handle} className={classes.margin} component={Link} size="large" variant="outlined" to={{pathname: destination,state:{user:newUser, g:false}}}> Submit</ColorButton>
+    <Link href='/User'>Back to User Page</Link>
         </div>
     </div>
+ );
 }
+}
+
+export default Edit;
