7021209ebadcfa4c5c94deb0f6812220fff8f94d
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-04-16 01:48:25 -0400

    Update SignUp.js

--- client/src/views/SignUp/SignUp.js
+++ client/src/views/SignUp/SignUp.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import { withRouter, Redirect } from 'react-router-dom';
-import { Flex, Box, Button, Heading, Text } from 'rebass';
+import { Flex, Box, Heading, Text } from 'rebass';
 import { Label, Input } from '@rebass/forms'
 import firebase from './config2';
 import UserProfile from './UserState';
 import axiosPath from '../../axiosRequests';
 import SignUpWithGoogle from './SignUpWithGoogle';
+import logo from '../../assets/logo.svg';
+import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 import './SignUp.css';
+import Button from "@material-ui/core/Button"
+import background from '../../assets/moonbackground.jpg';
+import { useForm } from 'react-hook-form';
+
+const ColorButton = withStyles(theme => ({
+  root: {
+      borderRadius: 20,
+      fontSize: 12,
+      padding: '3px 10px',
+      border: '1px solid',
+      backgroundColor: '#E28222',
+    '&:hover': {
+      backgroundColor: '#C6721D',
+    },
+  },
+}))(Button);
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
 class SignUp extends Component {
     constructor() {
         super();
@@ -27,19 +53,40 @@ return b;
        };
 
 
+       LoginPage=()=>{
+         console.log("click clack");
+        this.props.history.push('/Login');
+      }
+
+      
 handleInputChange = (event) => {
    this.setState({ [event.target.name]: event.target.value });
  };
 handleSubmit = async (event) => {
    event.preventDefault();
+   if(!(UserProfile.getLocalStorageEmail()==='' ||UserProfile.getLocalStorageEmail()==='null' || UserProfile.getLocalStorageEmail()===null || UserProfile.getLocalStorageEmail()===undefined || UserProfile.getLocalStorageEmail()==='undefined'))
+              alert("You are already logged in with email "+UserProfile.getLocalStorageEmail()+".  Please log out before creating a new account");
+   else{
+              if(this.state.email.length===0)
+   alert("Please provide an email");
+   else{
    const obj=await this.log2();
    if(obj!==undefined){
    alert("Already a user with this email");
    return (<Redirect to={{pathname: '/Login'}}></Redirect>);
    }
    else{
-
    const { email, password } = this.state;
+   if(password===undefined || password.length<6)
+   alert("Please use a password of 6 or more characters");
+   else{
+     if(this.state.name===undefined || this.state.name.length===0)
+     alert("Please provide a name");
+     else{
+       if(this.state.pob===undefined ||this.state.pob.length===0)
+       alert("Please provide a birth location");
+       else{
+console.log(this.state.pob);
 firebase
      .auth()
      .createUserWithEmailAndPassword(email, password)
@@ -68,7 +115,6 @@ firebase
         TimeOfBirth: this.state.tob,
         LocationOfBirth: this.state.pob,
         Email: this.state.email,
-        Password: this.state.password
     }
     await axiosPath.makeCreateRequest('personal/', axiosUser);
     console.log("SUccess");
@@ -85,6 +131,11 @@ firebase
        this.setState({ error: error });
      });
     }
+  }
+  }
+}
+}
+   }
  };
 
 
@@ -100,70 +151,99 @@ firebase
    const { email, password, error , name, dob, tob, pob} = this.state;
 console.log(10);
    return (
-       <div>
-       <Flex>
-         <Box>
-           <Heading>Register</Heading>
-         </Box>
-       </Flex>
-       {error ? (
-         <Flex>
-           <Box>
-             <Text>{error.message}</Text>
-           </Box>
-         </Flex>
-       ) : null}
-       <Flex>
-         <Box>
+       <div className="SignIn">
+            <header className="SignIn-header" style={{backgroundImage: `url(${background})` }}>
+                <h1 className="signin-title">
+                    User Information
+                </h1>
+               
+       
+                  <div className="Signin-card">  <p></p> 
+
            <form onSubmit={this.handleSubmit}>
-             <Input type="text" name="email" placeholder="Email" value={email} onChange={this.handleInputChange} />
-             <Input
+
+           <div>
+            <input
+               type="text"
+               name="name"
+               placeholder="name"
+
+               value={name}
+               onChange={this.handleInputChange}
+             />
+             </div>
+
+<div>
+           <input type="text" name="email" placeholder="Email" value={email} onChange={this.handleInputChange} />
+           </div>
+             <div>
+             <input
                type="password"
                name="password"
                placeholder="Password"
+
                value={password}
                onChange={this.handleInputChange}
              />
-            <Input
+             </div>
+
+
+             <div>
+            <input
+               type="text"
+               name="pob"
+               placeholder="place of birth"
+
+               value={pob}
+               onChange={this.handleInputChange}
+             />
+             </div>
+
+             <div>
+            <input
+               type="text"
+               name="tob"
+               placeholder="time of birth"
+                      
+               value={tob}
+               onChange={this.handleInputChange}
+             />
+             </div>
+
+             <div>
+            <input
                type="date"
                name="dob"
                placeholder="date of birth"
                value={dob}
                onChange={this.handleInputChange}
              />
+             </div>
 
-            <Input
-               type="text"
-               name="tob"
-               placeholder="time of birth"
-               value={tob}
-               onChange={this.handleInputChange}
-             />
 
-            <Input
-               type="text"
-               name="pob"
-               placeholder="place of birth"
-               value={pob}
-               onChange={this.handleInputChange}
-             />
-
-            <Input
-               type="text"
-               name="name"
-               placeholder="name"
-               value={name}
-               onChange={this.handleInputChange}
-             />
-
-             <Button children="Register" />
+             <p style={{marginBottom:-15}}></p>
+             <div>
+             <ColorButton children="Register" className={useStyles.margin} size="large" onClick={this.handleSubmit}/>
+</div>
            </form>
-         </Box>
-       </Flex>
-       or 
+<p style={{marginTop: 5, marginBottom: 10, fontSize:25}}>or</p>
        <SignUpWithGoogle></SignUpWithGoogle>
-
+           </div>
+           
+       
+       
+       <p style={{marginBottom:5}}>
+                    <br></br><br></br>
+                    Already a User?
+                </p>
+                <div>
+                <ColorButton onClickCapture={this.LoginPage}className={useStyles.margin} size="large" variant="outlined" >GO LOGIN NOW</ColorButton>
+                        </div>
+</header>
        </div>
+
+       
+                
    );
    console.log(this.state.email);
 
