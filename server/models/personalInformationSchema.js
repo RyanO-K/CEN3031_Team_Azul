3f160f37ac462826ca46afef935831a6dd2a4a40
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-04-17 15:20:35 -0400

    Update personalInformationSchema.js

--- server/models/personalInformationSchema.js
+++ server/models/personalInformationSchema.js
@@ -10,7 +10,8 @@ const personalInformationData = new mongoose.Schema({
   Birthday:{type: String},
   TimeOfBirth:{type: String},
   LocationOfBirth:{type: String,required:true},
-  Email:{type:String,required:true,unique:true}
+  Email:{type:String,required:true,unique:true},
+  House:{type:String}
   },{
     collection: 'Users'
   });
