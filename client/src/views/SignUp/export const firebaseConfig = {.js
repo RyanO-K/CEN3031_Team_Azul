3370013e3af782847a87b5fd14661e060754aa8f
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-04-19 20:00:42 -0400

    Delete export const firebaseConfig = {.js

--- client/src/views/SignUp/export const firebaseConfig = {.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCfFyuEwG1ZWXQ5zS6sx35cyFFGpFDrOPo",
-  authDomain: "moon-269221.firebaseapp.com",
-  databaseURL: "https://moon-269221.firebaseio.com",
-  projectId: "moon-269221",
-  storageBucket: "moon-269221.appspot.com",
-  messagingSenderId: "653554172936",
-  appId: "1:653554172936:web:50548287ba1ef095babdbd"
-};
