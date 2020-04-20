2480bc2921600d44e624ab3f7faed848fc9757b2
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-04-19 22:42:23 -0400

    Update ResetPassword.js

--- client/src/views/SignUp/ResetPassword.js
+++ client/src/views/SignUp/ResetPassword.js
@@ -5,6 +5,10 @@ import React, { Component } from 'react';
 import Button from "@material-ui/core/Button";
 import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import background from '../../assets/moonbackground.jpg';
+
+
+
+//button styling
 const ColorButton = withStyles(theme => ({
     root: {
         borderRadius: 20,
@@ -25,41 +29,44 @@ const ColorButton = withStyles(theme => ({
   }));
   
 
+  //password reset class (shows the password resetting on the front end)
 class PasswordReset extends Component{
+  //constructor sets the email's state (and the error state)
 constructor(){
     super();
     this.state={email:'', error:''};
 }
+
+//when a user changes their input email, this changes the state of their input accordingly
 handleInputChange = (event) => {
-    console.log(this.state.email);
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  
+  //when  user submits their email for a password reset email, this method is called
 handleSubmit = async (event) => {
     
     event.preventDefault();
     const { email, error} = this.state;
-    console.log(email);
 
 var auth = firebase.auth();
 var emailAddress = email;
 
+//firebase sends an email to reset the password of the user who forgot their email and if there is such a user, they are alerted when the email is sent
 auth.sendPasswordResetEmail(email).then(function() {
     alert("A reset email has been sent");
-  // Email sent.
 }).catch(function(error) {
-  console.log(error.message);
+  //otherwise the error message alerts the user
   let mes='';
   if(error.message.indexOf('There is no user record corresponding to this identifier.')>=0)
    mes=error.message+" Or You may have signed up with google sign in"; 
  else
  mes=error.message;
    alert(mes);
-  // An error happened.
 });
 }
 
+
+//the render function shows the reset email information and has the background image
 render(){
 const { email, error } = this.state;
 return(
