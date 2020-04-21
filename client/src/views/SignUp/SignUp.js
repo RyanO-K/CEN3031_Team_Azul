42646622b23beed7a8a5cfe682300136b8b7b96b
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-04-20 21:38:50 -0400

    Update SignUp.js

--- client/src/views/SignUp/SignUp.js
+++ client/src/views/SignUp/SignUp.js
@@ -99,9 +99,9 @@ handleSubmit = async (event) => {
        alert("Please provide a valid birth date");
        else{
          //we restrict this email usage because when this email appears on the backend get request, emails are sent to users
-         if(this.state.email.indexOf('Admin@admin.com2')>=0)
-         alert("Please provide a valid email");
-        else{
+        // if(this.state.email.indexOf('Admin@admin.com2')>=0)
+         //alert("Please provide a valid email");
+        //else{
           //if user chooses undefined as location of birth, alert them that they cannot do that
           if(this.state.pob==='undefined')
             alert("Please provide a valid location of birth (or leave it blank)");
@@ -173,7 +173,7 @@ firebase.auth().createUserWithEmailAndPassword(email, password).then(async (user
      .catch((error) => {
        alert(error);
      });
-    }
+   // }
   }
 }
   }
